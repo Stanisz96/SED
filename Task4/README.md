0c66d8722bc1517f53db0e074a0f62125434fc68
Michał Staniszewski <stanisz96@gmail.com>
2020-04-20 17:28:20 +0200

    Update Task4

--- Task4/README.md
+++ Task4/README.md
@@ -5,8 +5,8 @@ Use wine dataset from _Task 3_ to perform following points:
 * [x] name columns, using the description,
 * [x] create full decision tree (leafs with elements of one class),
 * [x] draw full decision tree,
-* [ ] check accuracy of the full tree by repeated substitution and cross-validation,
-* [ ] use the _cp_ table to select the optimal tree, draw it and compare the results of its
+* [x] check accuracy of the full tree by repeated substitution and cross-validation,
+* [ ] use Minimal Cost-Complexity Pruning, draw it and compare the results of its
 accuracy with the full tree,
 * [ ] create tree for the first: 2, 3, 4, etc. variables - determine the most optimal tree each time,
 * [ ] plot the accuracy of the tree as a function of the number of used variables, as well 
@@ -46,5 +46,66 @@ Result of this decision tree:
 <img src="https://raw.githubusercontent.com/Stanisz96/SED/master/Task4/FullTree.png" height="100%" width="100%">
 
 
+### Check accuracy of the full decision tree by repeated substitution
+Using method `predict()` on _DecisionTreeClassifier_ object with fitted _wineData_ set, get array of predicted class:
+```
+cls_predict = pd.DataFrame()
+cls_predict["Class"] = wineData.Class.values
+cls_predict["Predicted"] = clf.predict(wineData.loc[:,wineData.columns != 'Class'].values)
+print(cls_predict)
+>      Class  Predicted
+  0        1          1
+  1        1          1
+  ..     ...        ...
+  175      3          3
+  176      3          3
+  177      3          3
+```
+Then, using created functions `CM()` (for calculate confusion matrix) and `derivationsCM()` (to calculate derivations from a confusion matrix) - get _DataFrame_ object:
+```
+confusion_matrix = CM(cls_predict)
+derivations_CM = derivationsCM(confusion_matrix)
+print(derivations_CM)
+>    Class  ACC  TP   TN  TPR  FPR
+  0      1  1.0  59  119  1.0  0.0
+  1      2  1.0  71  107  1.0  0.0
+  2      3  1.0  48  130  1.0  0.0
+```
+Thus, it follows that a decision tree that is trained on the same data as it has been checked has 100% accuracy.
+
+
+
+### Check accuracy of the full decision tree by _k-fold cross-validation_
+Because of small amount of data, constant ___ is set to 5. To divide dataset is used object `KFold` from _Sklearn_ library.
+Calculating means for derivations of confusion matrix get result for full decision tree:
+```
+print(sum(d_CM)/5)
+>    Class       ACC    TP     TN       TPR       FPR
+  0    1.0  0.991011  58.6  117.8  0.993220  0.010084
+  1    2.0  0.982022  69.0  105.8  0.971831  0.011215
+  2    3.0  0.988764  47.0  129.0  0.979167  0.007692
+```
+Mean accuracy for cross-validation method is 1% lower then for repeated substitution. Accuracy is close to _1.00_ and
+it is possibility that classifier is overfitting the training data. Good way is to check data for 10-fold cross-validation:
+```
+print(sum(d_CM)/10)
+>    Class       ACC    TP     TN       TPR       FPR
+  0    1.0  0.992697  58.3  118.4  0.988136  0.005042
+  1    2.0  0.991573  70.3  106.2  0.990141  0.007477
+  2    3.0  0.996629  47.7  129.7  0.993750  0.002308
+```
+Accuracy is even higher then for 5-fold cross-validation. So it can be assumed that classifier is set good for this dataset.
+
+### Minimal Cost-Complexity Pruning
+To perform _Minimal Cost-Complexity Pruning_ are used methods on _DecisionTreeClassifier_ object <sup> _[[2]]_</sup>&nbsp;:
+* `cost_complexity_pruning_path()` - compute the pruning path during Minimal Cost-Complexity Pruning,
+* `decision_path()` - return the decision path in the tree.
+
+
+
+
+
+
 
 [1]: https://www.graphviz.org/
+[2]: https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html#sklearn.tree.DecisionTreeClassifier
--- Task4/main.py
+++ Task4/main.py
@@ -1,7 +1,11 @@
 from sklearn import tree
 import pandas as pd
+# from sklearn.metrics import confusion_matrix
 import graphviz
+import numpy as np
 import os
+from sklearn.model_selection import KFold
+
 os.environ["PATH"] += os.pathsep + 'D:/ProgramFiles/Graphviz/release/bin'
 
 file = open("wine.data", "r")
@@ -26,11 +30,104 @@ wineData = wineData.astype({"Class": "int64", "Proline": "int64"})
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(wineData.loc[:,wineData.columns != 'Class'].values, wineData['Class'].values)
 
-### Draw full decision tree
-column_names = wineData.loc[:,wineData.columns != 'Class'].columns.values
-class_names = ["1","2","3"]
-graph_data = tree.export_graphviz(clf, out_file=None, feature_names=column_names, filled=True,
-                                  rounded=True, special_characters=True, class_names=class_names)
-graph = graphviz.Source(graph_data, format='png')
-graph.render("FullTree")
+# ### Draw full decision tree
+# column_names = wineData.loc[:,wineData.columns != 'Class'].columns.values
+# class_names = ["1","2","3"]
+# graph_data = tree.export_graphviz(clf, out_file=None, feature_names=column_names, filled=True,
+#                                   rounded=True, special_characters=True, class_names=class_names)
+# graph = graphviz.Source(graph_data, format='png')
+# graph.render("FullTree")
 
+
+### Check accuracy of the full tree
+cls_predict = pd.DataFrame()
+cls_predict["Class"] = wineData.Class.values
+cls_predict["Predicted"] = clf.predict(wineData.loc[:,wineData.columns != 'Class'].values)
+
+
+def CM(data):
+    cls_n = data['Class'].max()
+    confusion_matrix = np.zeros([cls_n, cls_n])
+
+    for i in range(cls_n):
+        for j in range(cls_n):
+            confusion_matrix[i, j] = sum(data.Predicted[data.Class == i + 1] == j + 1)
+
+    return confusion_matrix
+
+
+def derivationsCM(CM):
+    tp = [0 for x in range(np.size(CM, 0))]
+    fp = [0 for x in range(np.size(CM, 0))]
+    fn = [0 for x in range(np.size(CM, 0))]
+    tn = [0 for x in range(np.size(CM, 0))]
+    acc = [0 for x in range(np.size(CM, 0))]
+    tpr = [0 for x in range(np.size(CM, 0))]
+    fpr = [0 for x in range(np.size(CM, 0))]
+
+    for i in range(np.size(CM, 0)):
+        for j in range(np.size(CM, 0)):
+            if i == j:
+                tp[i] = CM[i, j]
+            fp[i] += CM[j,i]
+            fn[i] += CM[i,j]
+        fp[i] -= tp[i]
+        fn[i] -= tp[i]
+        tn[i] = np.sum(CM) - tp[i] - fp[i] - fn[i]
+        acc[i] = (tp[i]+tn[i])/(tp[i]+tn[i]+fp[i]+fn[i])
+        tpr[i] = tp[i]/(tp[i]+fn[i])
+        fpr[i] = fp[i]/(fp[i]+tn[i])
+
+    derivations = pd.DataFrame({"Class": [],"ACC": [],"TP": [],"TN": [],"TPR": [],"FPR": []})
+    for i in range(np.size(CM, 0)):
+        temp = pd.Series({"Class": i+1,"ACC": acc[i],"TP": tp[i],"TN": tn[i],"TPR": tpr[i],"FPR": fpr[i]})
+        derivations = derivations.append(temp, ignore_index=True)
+
+    derivations = derivations.astype({"Class": 'int64',"TP": 'int64',"TN": 'int64'})
+    return derivations
+
+
+confusion_matrix = CM(cls_predict)
+derivations_CM = derivationsCM(confusion_matrix)
+
+
+### Full decision tree with cross-validation
+kf = KFold(n_splits=10,shuffle=True)
+k_errors = [0,0,0,0,0,0,0,0,0,0]
+d_CM = [0,0,0,0,0,0,0,0,0,0]
+n = 0
+
+for train_idx, test_idx in kf.split(wineData):
+    wineData_Train = wineData.loc[train_idx, :]
+    wineData_Test = wineData.loc[test_idx, :]
+    # train data
+    TRAIN = tree.DecisionTreeClassifier()
+    TRAIN = TRAIN.fit(wineData_Train.loc[:,wineData_Train.columns != 'Class'].values, wineData_Train['Class'].values)
+    # predict class
+    wineDataPredict = pd.DataFrame()
+    wineDataPredict["Class"] = wineData.Class.values
+    wineDataPredict["Predicted"] = TRAIN.predict(wineData.loc[:, wineData.columns != 'Class'].values)
+    k_errors[n] = sum(wineDataPredict.Class.values != wineDataPredict.Predicted.values)
+    confusion_matrix = CM(wineDataPredict)
+    d_CM[n] = derivationsCM(confusion_matrix)
+    n += 1
+
+print(sum(d_CM)/10)
+
+
+# ### Minimal Cost-Complexity Pruning
+# clf_min = tree.DecisionTreeClassifier()
+# # clf_min = clf_min.decision_path(wineData.loc[:,wineData.columns != 'Class'].values)
+# # clf_min = clf_min.cost_complexity_pruning_path(wineData.loc[:,wineData.columns != 'Class'].values, wineData['Class'].values)
+# print(clf_min.fit(wineData.loc[:,wineData.columns != 'Class'].values, wineData['Class'].values).decision_path(wineData.loc[:,wineData.columns != 'Class'].values))
+# # clf_min = clf_min.fit(wineData.loc[:,wineData.columns != 'Class'].values, wineData['Class'].values)
+
+
+
+# ### Draw full decision tree
+# column_names = wineData.loc[:,wineData.columns != 'Class'].columns.values
+# class_names = ["1","2","3"]
+# graph_data = tree.export_graphviz(clf_min, out_file=None, feature_names=column_names, filled=True,
+#                                   rounded=True, special_characters=True, class_names=class_names)
+# graph = graphviz.Source(graph_data, format='png')
+# graph.render("MinTree")
