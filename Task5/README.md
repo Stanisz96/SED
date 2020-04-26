a6f2052e9cd8673788b1d888b068c452ce78cdce
Michał Staniszewski <stanisz96@gmail.com>
2020-04-26 20:48:08 +0200

    Update Task5

--- Task5/README.md
+++ Task5/README.md
@@ -2,8 +2,52 @@
 ## The content of the task
 Use _Iris_ dataset and perform following points:
 * [x] load _Iris_ dataset,
-* [x] divide dataset to train data (~80%) and test data (~20%),
-* [ ] use _Bagging_ classifier with _LDA_ estimator for number of estimators: 1, 2, 5, 10, 20, 50,
-* [ ] use _Bagging_ classifier with _Decision Tree_ estimator for number of estimators: 1, 2, 5, 10, 20, 50.
+* [x] sample max 80% data to train classifier,
+* [x] use _Bagging_ classifier with _LDA_ estimator for number of estimators: 1-50,
+* [x] use _Bagging_ classifier with _Decision Tree_ estimator for number of estimators: 1-50.
 
 
+
+### _Bagging_ classifier with _LDA_ estimator
+Using obejct _BaggingClassifier_ from _Sklearn_ library can be perform _Bagging classifier_ with any estimators.
+For this point is used _LDA_ estimator. Created function `get_clf_predicted()` with arguments for object of estimator and number of estimators - return _DataFrame_ with columns for class and predicted class:
+```
+print(get_clf_predict(LDA(), 5))
+>      Class  Predicted
+  0        0          0
+  1        0          0
+  2        0          0
+  ..     ...        ...
+  148      2          2
+  149      2          2
+```
+
+This function create object _BaggingClassifier_ and set parameters such as: estimator, number of estimators, part of the sample data.
+Then, on created object is used method `fit()` to build a bagging ensemble of estimators from the training. Next task is to predict classes of given data. Last step for this function is to create and return _DataFrame_ object.
+
+Using loop, was created object _DataFrame_ with number of wrong predicted labels in % and number of estimators:
+```
+print(err_LDA_table)
+>    Error [%]  Number of estimators
+ 0        0.00                   1.0
+ 1        2.00                   2.0
+ 2        2.00                   3.0
+ ..        ...                   ...
+ 48       0.00                  49.0
+ 49       0.67                  50.0
+```
+Created plot with this data:
+
+<img src="https://raw.githubusercontent.com/Stanisz96/SED/master/Task5/LDA.png" height="75%" width="75%">
+
+
+### _Bagging_ classifier with _Decision Tree_ estimator
+To create _Bagging classifier_ with _Decision tree_ estimator - can be followed the instructions as in previous point.
+
+Created plot for Decision Tree:
+
+<img src="https://raw.githubusercontent.com/Stanisz96/SED/master/Task5/Tree.png" height="75%" width="75%">
+
+
+### Conclusion 
+As seen in the charts - _Bagging classifier_ is not good chooise to improve quality for _LDA_ estimator. However _Bagging classifier_ returns better results for _Decision Tree_ estimator.
--- Task5/main.py
+++ Task5/main.py
@@ -1,5 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.ensemble import BaggingClassifier
+import matplotlib.pyplot as plt
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn import tree
 import graphviz
 import pandas as pd
@@ -12,13 +14,50 @@ class_names = ['setosa', 'versicolor', 'virginica']
 irisData = pd.DataFrame({'Class': iris[1], features_names[0]: iris[0][:,0], features_names[1]: iris[0][:,1],
                          features_names[2]: iris[0][:,2], features_names[3]: iris[0][:,3]})
 
-PU_idx = np.random.rand(len(irisData)) < 0.8
-irisData_train = irisData[PU_idx]
-irisData_test = irisData[~PU_idx]
+# PU_idx = np.random.rand(len(irisData)) < 0.8
+# irisData_train = irisData[PU_idx]
+# irisData_test = irisData[~PU_idx]
 
-clf = BaggingClassifier(n_estimators=50)
-clf = clf.fit(irisData_train.loc[:,irisData_train.columns != 'Class'].values, irisData_train['Class'].values)
-predicted_Data = clf.predict(irisData_test.loc[:,irisData_test.columns != 'Class'].values)
-print(predicted_Data)
-print(irisData_test['Class'].values)
+def get_clf_predict(estimator, n_estimators):
+    clf = BaggingClassifier(base_estimator=estimator, n_estimators=n_estimators, max_samples=0.8)
+    clf = clf.fit(irisData.loc[:,irisData.columns != 'Class'].values, irisData['Class'].values)
+    predicted_Data = clf.predict(irisData.loc[:,irisData.columns != 'Class'].values)
+    temp = {'Class': irisData.Class.values, 'Predicted': predicted_Data}
+    clfData = pd.DataFrame(data=temp)
+    clfData = clfData.set_index([pd.Index(irisData.index.values)])
+    return clfData
 
+
+err_LDA_table = pd.DataFrame()
+err_Tree_table = pd.DataFrame()
+
+for i in range(1,51):
+    tempData_LDA = get_clf_predict(LDA(), i)
+    tempData_Tree = get_clf_predict(None, i)
+    errLDA = sum(tempData_LDA.Class.values != tempData_LDA.Predicted.values) / len(tempData_LDA.Class.values) * 100
+    errTree = sum(tempData_Tree.Class.values != tempData_Tree.Predicted.values)/len(tempData_Tree.Class.values) * 100
+    err_LDA_table = err_LDA_table.append({'Number of estimators': i, 'Error [%]': round(errLDA, 2)},ignore_index=True)
+    err_Tree_table = err_Tree_table.append({'Number of estimators': i, 'Error [%]': round(errTree, 2)},ignore_index=True)
+
+print(err_LDA_table)
+print(err_Tree_table)
+
+# fig, ax = plt.subplots()
+# fig.set_figheight(6)
+# fig.set_figwidth(8)
+# ax.plot(err_LDA_table['Number of estimators'].values, err_LDA_table['Error [%]'].values, marker='o', drawstyle="steps-post")
+# ax.set_xlabel("Number of estimators")
+# ax.set_ylabel("Error of wrong predicted [%]")
+# ax.set_title("Dependence of error on number of estimators for LDA")
+# plt.savefig("LDA.png",dpi=150)
+# plt.close()
+
+# fig, ax = plt.subplots()
+# fig.set_figheight(6)
+# fig.set_figwidth(8)
+# ax.plot(err_Tree_table['Number of estimators'].values, err_Tree_table['Error [%]'].values, marker='o', drawstyle="steps-post")
+# ax.set_xlabel("Number of estimators")
+# ax.set_ylabel("Error of wrong predicted [%]")
+# ax.set_title("Dependence of error on number of estimators for Decision Tree")
+# plt.savefig("Tree.png",dpi=150)
+# plt.close()
