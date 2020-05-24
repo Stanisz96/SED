7b54870dc6abf7587e6ef54729c7111a9d06fc13
Michał Staniszewski <stanisz96@gmail.com>
2020-05-24 19:38:15 +0200

    Update Task7

--- Task7/README.md
+++ Task7/README.md
@@ -2,7 +2,7 @@
 
 ## The content of the task
 List to do:
-* [x] Interpret animal characteristic data using function `heatmap()` from library _seaborn_,
+* [x] Interpret animal characteristic data using function `clastermap()` from library _seaborn_,
 * [ ] Using _K-means_ method - recognize clasters in the _Iris_ dataset. Test all combinations for attributes.
 
 ## Interpret animal characteristic data using function `heatmap()` from library _seaborn_
@@ -44,4 +44,90 @@ The least similar animals are: ant and eagle.
 
 ## Using _K-means_ method - recognize clasters in the _Iris_ dataset
 
+### K-Means method
+Probably the most popular, non-hierarchical cluster analysis algorithm is
+the K-means algorithm. Assignment of n objects to a given number of K clusters
+is carried out independently for each K value - not based on previously determined
+smaller or larger clusters [[1], 385].
 
+Algorithm for K-Means:
+* Randomly initialize _N_ objects in _K_ clusters. Where function _C<sub>K</sub><sup>(i)</sup>_ describe this distribution,
+* For each group cluster K - calculate mean vector:
+    <img src="https://latex.codecogs.com/gif.latex?%5Cbar%7B%5Cmathbf%7Bx%7D%7D_k%2C%5C%3A%20%5C%3A%20where%5C%3A%20k%3D1%2C2%2C...%2CK"></img>
+* Regroup objects in K clusters, so that they meet the condition:
+    
+    <img src="https://latex.codecogs.com/gif.latex?C%5E%7B%28l%29%7D_K%28i%29%3Darg%5C%2C%20%5Cunderset%7B1%5Cleq%20k%5Cleq%20K%7D%7Bmin%7D%5C%2C%20%5Crho_2%28%5Cmathbf%7Bx_i%7D%2C%5Cmathbf%7B%5Cbar%7Bx%7D%7D_k%29"></img>
+* Repeat second and third step until the assignment of objects to clusters remains unchanged.
+
+### Iris data visualization (PCA)
+Iris data has 4 attributes, therefore it can't be projected on plot. To enable data visualization, is needed to reduce
+the variables to 2. For this is used _Principal component analysis (PCA)_.
+First create object `PCA()` from _sklearn.decomposition_. Then use method `fit_transform()`. It will return data with new variables.
+
+One the plot below is Iris dataset - transformed to 2 variables and divided into original classes assigned to data.
+
+<img src="https://raw.githubusercontent.com/Stanisz96/SED/master/Task7/img/pca_class.png" height="75%" width="75%">
+
+Data are divided in to three classes. Class 1 and 2 are quite close and it is probably possible to divide dataset to more then 3 classes. It will be presented later.
+
+
+### K-Means method on Iris dataset
+Using `KMeans()` object from _sklearn_ library, with parameter _n_components = 2_ is performed KMeans algorithm.
+To visualize data is used as earlier - `PCA()`. 
+
+On the plot below it can be seen representation for 3 clusters assigned using KMeans method for Iris dataset.
+
+<img src="https://raw.githubusercontent.com/Stanisz96/SED/master/Task7/img/pca_kmeans_3.png" height="75%" width="75%">
+
+There is slight difference of data assigned to cluster 1 and 2 between KMeans method and original data.
+
+
+### Search for optimized number of clusters (K)
+To find the optimal K was used method based on maximization _gap statistic_.
+
+First - to perform this method - create dataset with the same size as Iris dataset, using uniform distribution - where _min_ and _max_
+values for attributes are based on _min_ and _max_ values in Iris dataset. Perform `KMeans()` method on this data for number of cluster from 1 to 10.
+From every object _KMeans()_ get value defined as sum of squared distances of samples to their closest cluster center. Then, use logarithm on this value.
+Perform this 20 times and then calculate mean for sum of squared distances and calculate standard deviation. Created variables are represented as [[1], 389]:
+
+<img src="https://latex.codecogs.com/gif.latex?W%5E%7B*%7D_%7BK%7D%5C%3A%5C%3A%20and%5C%3A%5C%3A%20s%5E%7B*%7D_%7BK%7D"></img>
+
+Calculated values of sums of squared distances of samples to their closest cluster center for different number of cluster are shown on plot below.
+This values are not shown in logarithmic scale.
+
+<img src="https://raw.githubusercontent.com/Stanisz96/SED/master/Task7/img/w_value.png" height="75%" width="75%">
+
+<br></br>
+_Gap statistic_ is defined as:
+
+<img src="https://latex.codecogs.com/gif.latex?Gap%28K%29%3DW%5E%7B*%7D_%7BK%7D-W_%7BK%7D"></img>
+
+Using calculated values - results are:
+
+<img src="https://raw.githubusercontent.com/Stanisz96/SED/master/Task7/img/gap_statistic.png" height="75%" width="75%">
+
+The optimal number of clusters using this method is first K which meets the condition [[2], 2]:
+
+<img src="https://latex.codecogs.com/gif.latex?Gap%28K%29%5Cgeq%20Gap%28k&plus;1%29-s_%7Bk&plus;1%7D%2C%5C%3A%20where%5C%2C%20s_%7Bk&plus;1%7D%3D%5Csqrt%7B1&plus;1/N%7D%5Ccdot%20s%5E*_%7Bk&plus;1%7D"></img>
+
+Calculated optimal number of clusters is 4. It can be seen on visualization using _PCA_:
+
+<img src="https://raw.githubusercontent.com/Stanisz96/SED/master/Task7/img/pca_kmeans_4.png" height="75%" width="75%">
+
+
+### Optimal number of clusters for every combinations of attributes in Iris dataset
+To calculate optimal number of clusters for every combinations of attributes in Iris dataset is used method in point below.
+To extract combinations of attributes from array is used function `combinations()` from library_itertools_.
+
+<img src="https://raw.githubusercontent.com/Stanisz96/SED/master/Task7/img/combination_iris.png" height="75%" width="75%">
+
+On the plot above:
+* horizontal axis means: number of attributes used multiply by 10 and added combination number,
+* vertical axis means: optimal number of cluster, calculated using _gap statistic_ method,
+* results that mean optimal number of clusters for 2 attributes is 3, for 3 attributes is 4 and for 4 attributes is 4.
+
+Using few attributes, is harder to divide dataset to the same number of clusters then using more attributes. However, too much attributes may not help to optimally divide dataset into more numbers of clusters.
+
+
+[1]: https://www.researchgate.net/publication/292148701_Systemy_uczace_sie_Rozpoznawanie_wzorcow_analiza_skupien_i_redukcja_wymiarowosci
+[2]: https://core.ac.uk/download/pdf/12172514.pdf
--- Task7/main.py
+++ Task7/main.py
@@ -1,7 +1,13 @@
 import pandas as pd
+from sklearn.datasets import load_iris
+import numpy as np
 import seaborn as sb
 from sklearn.impute import KNNImputer
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+from yellowbrick.cluster import KElbowVisualizer
+from itertools import combinations
 
 # Read data from file animals.csv
 dataAnimal = pd.read_csv("animals.csv")
@@ -9,11 +15,262 @@ dataAnimal.columns = ["animal","warm-blooded","can fly","vertebrate","endangered
 dataAnimal = dataAnimal.fillna(1.5)
 dataAnimal = dataAnimal.rename(index=dataAnimal.animal)
 dataAnimal = dataAnimal.loc[:,dataAnimal.columns != 'animal']
-# imputer = KNNImputer(n_neighbors=2, weights="uniform")
-# dataAnimal.loc[:,dataAnimal.columns != 'animal'] = imputer.fit_transform(dataAnimal.loc[:,dataAnimal.columns != 'animal'].values)
-
 
 ax = sb.clustermap(dataAnimal)
 ax.savefig("img/heatmap.png")
-print(ax.dendrogram_row.reordered_ind)
-print(ax.dendrogram_col.reordered_ind)
+
+
+# K-Means
+dataAnimal = pd.read_csv("animals.csv")
+dataAnimal.columns = ["animal","warm-blooded","can fly","vertebrate","endangered","live in groups","have hair"]
+dataAnimal = dataAnimal.fillna(1.5)
+
+k_means = KMeans()
+## Elbow Method
+elbow_value_table = []
+elbow_score_table = []
+n_table = []
+n_elbow_poin = 0
+n_combinations = 0
+
+# print(dataAnimal.columns.values[dataAnimal.columns != 'animal'])
+for i in range(2,7):
+    com_attributes = list(combinations(dataAnimal.columns.values[dataAnimal.columns != 'animal'],i))
+    for attributes in com_attributes:
+        n_combinations += 1
+        elbowMethod = KElbowVisualizer(k_means,k=(1,2*i+1))
+        # print(dataAnimal[np.array(attributes)])
+        elbowMethod.fit(dataAnimal[np.array(attributes)].values)
+        # print(dataAnimal.loc[:,dataAnimal.columns != 'animal'])
+        # elbowMethod.fit(dataAnimal.loc[:,dataAnimal.columns != 'animal'])
+
+        if elbowMethod.elbow_score_ != 0:
+            n_elbow_poin += 1
+            elbow_value_table.append(elbowMethod.elbow_value_)
+            elbow_score_table.append(elbowMethod.elbow_score_)
+            n_table.append(i)
+        plt.close()
+
+print("combinations "+str(n_combinations))
+print("finded elbow "+str(n_elbow_poin))
+
+plt.hist2d(x=n_table, y=elbow_value_table,bins=6)
+# plt.plot(n_table,elbow_score_table, drawstyle="steps-post")
+plt.title("lol")
+plt.xlabel("number")
+plt.ylabel("value and score")
+plt.legend(["Optimal K"])
+plt.show()
+# elbowMethod.finalize()
+#
+# plt.savefig('img/elbowtest.png',dpi=150)
+
+
+
+## Fit kmeans
+k_means.fit(dataAnimal.loc[:,dataAnimal.columns != 'animal'])
+pred_k_means = k_means.predict(dataAnimal.loc[:,dataAnimal.columns != 'animal'])
+clusters_center = k_means.cluster_centers_
+
+
+# PCA
+reduced_dataAnimal = PCA(n_components=2).fit_transform(dataAnimal.loc[:,dataAnimal.columns != 'animal'])
+dataAnimal_PCA = pd.DataFrame(reduced_dataAnimal, columns=['PCA1','PCA2'])
+
+# Join PCA points and pred_k_means to dataAnimal
+dataAnimal["PCA1"] = dataAnimal_PCA.PCA1
+dataAnimal["PCA2"] = dataAnimal_PCA.PCA2
+dataAnimal["predicted"] = pred_k_means
+
+
+sb.lmplot(data=dataAnimal.loc[:,dataAnimal.columns != 'animal'],x='PCA1', y='PCA2', fit_reg=False, hue = 'predicted', palette = ['#eb6c6a','#ebe76a', '#6aeb6c','#6aebeb', '#6c6aeb','#eb6acf'])
+# plt.scatter(clusters_center[:, 0], clusters_center[:, 1], c='black', s=100, alpha=0.5)
+plt.savefig("img/test.png",dpi=150)
+plt.close()
+
+
+
+# Using K-means method - recognize clasters in the Iris dataset. Test all combinations for attributes.
+
+iris = load_iris(return_X_y=True)
+features_names = ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']
+class_names = ['setosa', 'versicolor', 'virginica']
+irisData = pd.DataFrame({'Class': iris[1], features_names[0]: iris[0][:,0], features_names[1]: iris[0][:,1],
+                         features_names[2]: iris[0][:,2], features_names[3]: iris[0][:,3]})
+
+
+
+## K-means
+
+# k_means = KMeans(n_clusters=3)
+# k_means.fit(irisData.loc[:,irisData.columns != 'Class'])
+# pred_k_means = k_means.predict(irisData.loc[:,irisData.columns != 'Class'])
+# clusters_center = k_means.cluster_centers_
+
+
+# Use KMeans method for (1,10) clusters
+K = 10
+N = 20
+W_table = []
+W_uniform_table = []
+k_table = []
+
+for n in range(N):
+    # generate uniform distribution data
+    features = []
+    temp_W_uniform = []
+    for i in range(len(features_names)):
+        temp = np.random.random_integers(np.amin(irisData.iloc[:, i + 1])*10, np.amax(irisData.iloc[:, i + 1])*10, irisData.index.stop)
+        features.append(temp/10)
+
+    dataUniform = pd.DataFrame({features_names[0]: features[0], features_names[1]: features[1],
+                                features_names[2]: features[2], features_names[3]: features[3]})
+    for i in range(1,11):
+        k_means_uniform = KMeans(n_clusters=i, init="random")
+        k_means_uniform.fit(dataUniform)
+        pred_k_means_uniform = k_means_uniform.predict(dataUniform)
+        temp_W_uniform.append(k_means_uniform.inertia_)
+    W_uniform_table.append(np.log(temp_W_uniform))
+
+W_uniform_table = np.array(W_uniform_table)
+W_uniform_table_mean = []
+std_uniform_table = []
+for i in range(K):
+   std_uniform_table.append(2*np.std(W_uniform_table[:,i]))
+   W_uniform_table_mean.append(np.sum(W_uniform_table[:,i])/N)
+print(std_uniform_table)
+
+for i in range(1,11):
+    k_means = KMeans(n_clusters=i, init="random")
+    k_means.fit(irisData.loc[:, irisData.columns != 'Class'])
+    pred_k_means = k_means.predict(irisData.loc[:, irisData.columns != 'Class'])
+    W_table.append(np.log(k_means.inertia_))
+    k_table.append(i)
+
+plt.plot(k_table,np.exp(W_table), linestyle='--', lw=1, marker='.', ms=12)
+plt.plot(k_table,np.exp(W_uniform_table_mean), linestyle='--',lw=1, marker='.', ms=12)
+plt.title("W value")
+plt.xlabel("K")
+plt.ylabel(r'$W_{K}-W_{K}$')
+plt.legend(["data from Iris dataset","data from Uniform dataset"])
+plt.grid(True)
+plt.savefig("img/w_value.png",dpi=150)
+plt.close()
+
+## Gap statistic
+W_table = np.array(W_table)
+W_uniform_table_mean = np.array(W_uniform_table_mean)
+
+gap = W_uniform_table_mean - W_table
+optimal_k = 0
+for k in range(K):
+    condition = gap[k+1] - std_uniform_table[k+1] * np.sqrt(1+1/N)
+    if gap[k] >= condition:
+        optimal_k = k+1
+        break
+
+print(optimal_k)
+
+plt.plot(k_table,gap,linestyle='--', lw=0.8, color="blue")
+plt.errorbar(k_table,gap,yerr=std_uniform_table, fmt='o', color="blue")
+plt.title("Gap Statistic")
+plt.xlabel("K")
+plt.ylabel(r'$W^{*}_{K}-W_{K}$')
+# plt.legend([""])
+plt.grid(True)
+plt.savefig("img/gap_statistic.png",dpi=150)
+plt.close()
+
+
+# PCA
+reduced_dataAnimal = PCA(n_components=2).fit_transform(irisData.loc[:,irisData.columns != 'Class'])
+dataAnimal_PCA = pd.DataFrame(reduced_dataAnimal, columns=['PCA1','PCA2'])
+
+# KMeans for optimal K
+k_means = KMeans(n_clusters=optimal_k, init="random")
+k_means.fit(irisData.loc[:, irisData.columns != 'Class'])
+pred_k_means = k_means.predict(irisData.loc[:, irisData.columns != 'Class'])
+
+# Join PCA points and pred_k_means to dataAnimal
+irisData["PCA1"] = dataAnimal_PCA.PCA1
+irisData["PCA2"] = dataAnimal_PCA.PCA2
+irisData["clusters"] = pred_k_means
+
+
+sb.lmplot(data=irisData.loc[:,irisData.columns != 'Class'],x='PCA1', y='PCA2', fit_reg=False, hue = 'clusters', palette = ['#eb6c6a','#ebe76a', '#6aeb6c','#6aebeb', '#6c6aeb','#eb6acf'])
+# plt.scatter(clusters_center[:, 0], clusters_center[:, 1], c='black', s=100, alpha=0.5)
+plt.savefig("img/pca_kmeans_"+str(optimal_k)+".png",dpi=150)
+plt.close()
+
+
+
+## combinations for iri
+
+N = 20
+K = 10
+global_optimal_k = []
+table = []
+for i in range(2,5):
+    print(i)
+    com_attributes = list(combinations(irisData.columns.values[irisData.columns != 'Class'],i))
+    n_combinations = 0
+    for attributes in com_attributes:
+        W_table = []
+        W_uniform_table = []
+        k_table = []
+        n_combinations += 1
+
+
+        for n in range(N):
+            # generate uniform distribution data
+            features = []
+            temp_W_uniform = []
+            for j in range(len(features_names)):
+                temp = np.random.random_integers(np.amin(irisData.iloc[:, j + 1])*10, np.amax(irisData.iloc[:, j + 1])*10, irisData.index.stop)
+                features.append(temp/10)
+
+            dataUniform = pd.DataFrame({features_names[0]: features[0], features_names[1]: features[1],
+                                        features_names[2]: features[2], features_names[3]: features[3]})
+            for j in range(1,11):
+                k_means_uniform = KMeans(n_clusters=j, init="random")
+                k_means_uniform.fit(dataUniform[np.array(attributes)].values)
+                pred_k_means_uniform = k_means_uniform.predict(dataUniform[np.array(attributes)].values)
+                temp_W_uniform.append(k_means_uniform.inertia_)
+            W_uniform_table.append(np.log(temp_W_uniform))
+
+        W_uniform_table = np.array(W_uniform_table)
+        W_uniform_table_mean = []
+        std_uniform_table = []
+        for j in range(K):
+           std_uniform_table.append(2*np.std(W_uniform_table[:,j]))
+           W_uniform_table_mean.append(np.sum(W_uniform_table[:,j])/N)
+
+        for j in range(1,11):
+            k_means = KMeans(n_clusters=j, init="random")
+            k_means.fit(irisData[np.array(attributes)].values)
+            pred_k_means = k_means.predict(irisData[np.array(attributes)].values)
+            W_table.append(np.log(k_means.inertia_))
+            k_table.append(i)
+
+        ## Gap statistic
+        W_table = np.array(W_table)
+        W_uniform_table_mean = np.array(W_uniform_table_mean)
+
+        gap = W_uniform_table_mean - W_table
+        optimal_k = 0
+        for k in range(K):
+            condition = gap[k+1] - std_uniform_table[k+1] * np.sqrt(1+1/N)
+            if gap[k] >= condition:
+                optimal_k = k+1
+                global_optimal_k.append(optimal_k)
+                table.append(i * 10 + n_combinations)
+                break
+
+
+plt.plot(table,global_optimal_k, marker='o', drawstyle="steps-post")
+plt.title("Optimal K for attributes combinations")
+plt.xlabel(r'$n_{attr}*10+c$')
+plt.ylabel('Optimal K')
+plt.grid(True)
+plt.savefig("img/combination_iris.png",dpi=150)
+plt.close()
